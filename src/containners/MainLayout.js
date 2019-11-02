e9a7b5ec1c966308562dd97d5864213bdf135743
Tui Tpk <tanamet.tpk@gmail.com>
2019-11-02 11:18:00 +0700

    + mainlayout

--- src/containners/MainLayout.js
+++ src/containners/MainLayout.js
@@ -1,20 +1,15 @@
 import React from 'react'
-import {Navbar, FormControl,Button ,Form} from 'react-bootstrap'
+import {Navbar, FormControl,Button ,Form, Nav} from 'react-bootstrap'
 
 const index = (props) => {
     return (
         <div>
-            <Navbar bg="light" variant="light">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-                <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Navbar bg="light" variant="light" >
+
+                <Navbar.Brand href="/"><div style={{color:"#109CF1"}}>Kaptuer</div></Navbar.Brand>
+                <Nav className="mr-auto" />
+
                 <Button variant="outline-primary">Search</Button>
-                </Form>
             </Navbar>
             {props.children}
         </div>
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -9,6 +9,7 @@ import store from '../../store'
 import { logOut } from '../../store/actions/user'
 import Loader from '../../components/Loader'
 import { initGA, logEvent, pageView } from '../../util/analytics/ga'
+import MainLayout from '../../containners/MainLayout'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../asset/css/main.css'
@@ -17,32 +18,35 @@ import '../../asset/css/customs.css'
 function App(props) {
 
   useEffect(()=>{
-    initGA()
-    pageView()
+    // initGA()
+    // pageView()
   })
 
-  if (!props.user.isLogin){
-    logEvent("appPage", "not login")
-    window.location.href = "https://kaptuer.com";
-    return(<Loader />)
-  }
+  // if (!props.user.isLogin){
+  //   logEvent("appPage", "not login")
+  //   window.location.href = "https://kaptuer.com";
+  //   return(<Loader />)
+  // }
 
-  const logout = () => {
-    props.logOut()
-    logEvent("nav", "logout")
-  }
+  // const logout = () => {
+  //   props.logOut()
+  //   logEvent("nav", "logout")
+  // }
 
   return (
-    <Provider store={store}>
-      <Router>
-        {!props.user.isLogin || <Navbar isLogin={props.user.isLogin} logout={logout} name={props.user.name}/> }
-        <Switch>
-          <Route path="/" exact component={ApplicationManagement} />
-          <Route path="/projects" component={ProjectManagement} />
-          <Route component={()=><div>error</div>} />
-        </Switch>
-      </Router>
-    </Provider>
+    // <Provider store={store}>
+    //   <Router>
+    //     {!props.user.isLogin || <Navbar isLogin={props.user.isLogin} logout={logout} name={props.user.name}/> }
+    //     <Switch>
+    //       <Route path="/" exact component={ApplicationManagement} />
+    //       <Route path="/projects" component={ProjectManagement} />
+    //       <Route component={()=><div>error</div>} />
+    //     </Switch>
+    //   </Router>
+    // </Provider>
+    <MainLayout>
+      test
+    </MainLayout>
   );
 }
 
